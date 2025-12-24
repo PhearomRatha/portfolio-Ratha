bd2ca1b087519b1891763c27757de27c4949a89d
Ratha <phearom.ratha.2822@rupp.edu.kh>
2025-12-24 11:06:27 +0700

    updae image

--- src/components/home/Hero.vue
+++ src/components/home/Hero.vue
@@ -78,7 +78,7 @@
           <!-- Main circular image -->
           <div class="absolute inset-2 rounded-full overflow-hidden border-4 border-white dark:border-zinc-800 shadow-2xl">
             <img 
-              src="/public/Image/profile.png" 
+              src="/public/Image/photo_2025-12-24_11-05-07.jpg" 
               alt="Alex Morgan" 
               class="w-full h-full object-cover"
             />
